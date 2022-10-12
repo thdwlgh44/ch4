efd2cf4dbdc29a7f3bcf6effd4aa081254f9ec06
jiho <thdwlgh44@naver.com>
2022-10-12 15:17:08 +0900

    spring board: commit day1012

---
+++ src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -0,0 +1,54 @@
+package com.fastcampus.ch4.controller;
+
+import com.fastcampus.ch4.domain.*;
+import com.fastcampus.ch4.service.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.*;
+
+import javax.servlet.http.*;
+import java.time.*;
+import java.util.*;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @Autowired
+    BoardService boardService;
+
+    @GetMapping("/list")
+    public String list(Integer page, Integer pageSize, Model m, HttpServletRequest request) {
+        if(!loginCheck(request))
+            return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
+
+        if (page==null) page=1;
+        if (pageSize==null) pageSize=10;
+
+        try {
+
+            int totalCnt = boardService.getCount();
+            PageHandler pageHandler = new PageHandler(totalCnt, page, pageSize);
+
+            Map map = new HashMap();
+            map.put("offset", (page-1)*pageSize);
+            map.put("pageSize", pageSize);
+
+            List<BoardDto> list = boardService.getPage(map);
+            m.addAttribute("list", list);
+            m.addAttribute("ph", pageHandler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
+    }
+
+    private boolean loginCheck(HttpServletRequest request) {
+        // 1. 세션을 얻어서
+        HttpSession session = request.getSession();
+        // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
+        return session.getAttribute("id")!=null;
+    }
+}
